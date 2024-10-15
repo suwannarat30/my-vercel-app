030b493597fdc8444d6e8036307a66f29684d213
suwannarat30 <suaqeiq@gmail.com>
2024-10-15 11:40:01 +0700

    Update Welcome message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+ <div className="App">
+ <header className="App-header">
+ <h1>Suwannarat App</h1>
+ <p> This is a simple React app deployed on Vercel. </p>
+ <a className="App-link" href="https://vercel.com" target="_blank"
+ rel="noopener noreferrer" > Learn about Vercel </a>
+ </header>
+ </div>
+ );
 }
-
 export default App;
